d48be6195ad032868e5cafb7073ace4263b33104
Dave Pacheco <dap@joyent.com>
2014-06-27 15:40:38 -0700

    finish wiring up --before, --after, and add test cases
    also fixes a RangeError seen with the JsonLineStream
    also adds --counters to "query" subcommand
    also fixes crash when emitting a numeric breakdown with no values

--- tests/dn/tst.scan_fileset.sh
+++ tests/dn/tst.scan_fileset.sh
@@ -19,3 +19,14 @@ function scan
 }
 
 . $(dirname $0)/scan_testcases.sh
+
+#
+# The "before" and "after" filters should prune the number of files scanned.
+# When comparing output, it's important to verify the correct number of records
+# returned as well as the expected number of files scanned.
+#
+scan --counters -b '__dn_ts[aggr=lquantize;step=86400]' 2>&1
+scan --counters --time-format=%Y/%m-%d \
+    --after 2014-05-02 --before 2014-05-03 2>&1
+scan --counters --time-format=%Y/%m-%d -b '__dn_ts[aggr=lquantize;step=60]' \
+    --after "2014-05-02T04:05:06.123" --before "2014-05-02T04:15:10" 2>&1
