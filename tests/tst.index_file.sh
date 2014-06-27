289aa33726d4da23c5d745e3b9cd4bb573d9e1d4
Dave Pacheco <dap@joyent.com>
2014-06-27 10:19:31 -0700

    reorganize test data directories in prep for testing --before, --after

--- tests/tst.index_file.sh
+++ tests/tst.index_file.sh
@@ -18,11 +18,12 @@ function scan
 
 # Try all the "scan" test cases with an index that should handle them all.
 dn index -c host,operation,req.caller,req.method,latency[aggr=quantize] \
-    $DATADIR/one.log $tmpfile
+    $DATADIR/2014/05-01/one.log $tmpfile
 . $(dirname $0)/scan_testcases.sh
 
 # That should have been pretty exhaustive, but try an index with a filter on it.
-dn index -f '{ "eq": [ "req.method", "GET" ] }' $DATADIR/one.log $tmpfile
+dn index -f '{ "eq": [ "req.method", "GET" ] }' \
+    $DATADIR/2014/05-01/one.log $tmpfile
 scan
 
 rm -f $tmpfile
--- tests/tst.scan_file.sh
+++ tests/tst.scan_file.sh
@@ -9,11 +9,11 @@ set -o errexit
 function scan
 {
 	echo "# dn scan" "$@"
-	dn scan "$@" $DATADIR/one.log
+	dn scan "$@" $DATADIR/2014/05-01/one.log
 	echo
 
 	echo "# dn scan --points" "$@"
-	dn scan --points "$@" $DATADIR/one.log | sort -d
+	dn scan --points "$@" $DATADIR/2014/05-01/one.log | sort -d
 	echo
 }
 
